6e606f5460cc24c9e59b39b3d334c16cd1a00fc9
F_I_L_I_N <neckvikstyle@mail.ru>
2025-01-03 20:07:12 +0500

    мой первый комит

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,17 @@ public class Main {
         Gryffindor people2 = new Gryffindor(" Гермиона ", " Грейнджер ", " Гриффиндор ", 20, 20, 89, 46, 68);
         Gryffindor people3 = new Gryffindor(" Рон ", " Уизли ", " Гриффиндор ", 10, 10, 79, 36, 58);
 
-        Slytherin people4 = new Slytherin(" Драко ", " Малфой ", " Слизерин ", 30, 30, 99, 56, 78);
-        Slytherin people5 = new Slytherin(" Грэхэм ", " Монтегю ", " Слизерин ", 20, 20, 99, 56, 78);
-        Slytherin people6 = new Slytherin(" Седрик ", " Диггори", " Слизерин ", 10, 10, 99, 56, 78);
+        Slytherin people4 = new Slytherin(" Драко ", " Малфой ", " Слизерин ", 30, 30, 32, 54, 87);
+        Slytherin people5 = new Slytherin(" Грэхэм ", " Монтегю ", " Слизерин ", 20, 20, 92, 43, 56);
+        Slytherin people6 = new Slytherin(" Седрик ", " Диггори", " Слизерин ", 10, 10, 65, 23, 34);
 
         Hufflepuff people7 = new Hufflepuff(" Захария ", " Смит ", " Пуффендуй ", 30, 30, 99, 56, 78);
-        Hufflepuff people8 = new Hufflepuff(" Седрик ", " Диггори ", " Пуффендуй ", 20, 20, 99, 56, 78);
-        Hufflepuff people9 = new Hufflepuff(" Джастин ", " Финч-Флетчли ", " Пуффендуй ", 10, 10, 99, 56, 78);
+        Hufflepuff people8 = new Hufflepuff(" Седрик ", " Диггори ", " Пуффендуй ", 20, 20, 34, 54, 12);
+        Hufflepuff people9 = new Hufflepuff(" Джастин ", " Финч-Флетчли ", " Пуффендуй ", 10, 10, 12, 34, 65);
 
-        Ravenclaw people10 = new Ravenclaw(" Чжоу ", " Чанг ", " Когтевран ", 30, 30, 99, 56, 78);
-        Ravenclaw people11 = new Ravenclaw(" Падма ", " Патил ", " Когтевран ", 20, 20, 99, 56, 78);
-        Ravenclaw people12 = new Ravenclaw(" Маркус ", " Белби ", " Когтевран ", 10, 10, 99, 56, 78);
+        Ravenclaw people10 = new Ravenclaw(" Чжоу ", " Чанг ", " Когтевран ", 30, 30, 78, 53, 65);
+        Ravenclaw people11 = new Ravenclaw(" Падма ", " Патил ", " Когтевран ", 20, 20, 23, 67, 23);
+        Ravenclaw people12 = new Ravenclaw(" Маркус ", " Белби ", " Когтевран ", 10, 10, 69, 23, 43);
         System.out.println(people1);
         people5.comparison(people6 ,people4);
         people1.comparison(people3, people2);
